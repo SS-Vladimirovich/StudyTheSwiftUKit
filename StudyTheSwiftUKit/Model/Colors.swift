53be179b276827175cdf68e692ad3f4e63f64986
Sergey <sv.sokolov716@gmail.com>
2022-06-01 16:03:26 +0300

    save

---
+++ StudyTheSwiftUKit/Model/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  StudyTheSwiftUKit
+//
+//  Created by Sergey Sokolov on 01.06.2022.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - Colors
+
+let primaryViewColor = UIColor.systemBlue
--- StudyTheSwiftUKit/View/LoginViewController.swift
+++ StudyTheSwiftUKit/View/LoginViewController.swift
@@ -9,29 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var loginButton = UIButton()
+    //MARK: - Компаненты загрузочного экрана
+
+    let label = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loginButton = UIButton(type: .roundedRect)
-        loginButton.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
-        loginButton.setTitle("Press me", for: .normal)
-        loginButton.setTitle("I`m press", for: .highlighted)
-        loginButton.addTarget(self, action: #selector(buttonInPress), for: .touchDown)
-        loginButton.addTarget(self, action: #selector(buttonIsTapped), for: .touchUpInside)
-        loginButton.setTitleColor( .green, for: .normal)
-        self.view.addSubview(loginButton)
+        //MARK: - Gradient View
 
-    }
+        let layer = CAGradientLayer()
+        layer.frame = view.bounds
+        layer.colors = [UIColor.systemCyan.cgColor, UIColor.systemIndigo.cgColor]
+        layer.startPoint = CGPoint(x: 0,y: 0)
+        layer.endPoint = CGPoint(x: 1,y: 1)
+        view.layer.addSublayer(layer)
 
-    @objc func buttonInPress(sender: UIButton) {
-        print("Button in press")
-    }
-
-    @objc func buttonIsTapped(sender: UIButton) {
-        print("Button is Tupped")
     }
 
 }
-
